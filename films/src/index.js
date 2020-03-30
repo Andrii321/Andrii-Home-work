22fe77928d072d4b9acb73f7506f12c303aa43a5
Andrii321 <60897026+Andrii321@users.noreply.github.com>
2020-03-30 15:17:32 +0300

    Add files via upload

---
+++ films/src/index.js
@@ -0,0 +1,20 @@
+const film = {
+  name: "Terminator-2. JudgementDay",
+  year: "1991",
+  genre: "action",
+  casherfish: "520$",
+  budget: "100$",
+  director: "James Cameron",
+  rating: "8,5 IMDb",
+  actors:
+    "Arnold Schwarzenegger, Linda Hamilton, Robert Patrick, Edward Furlong"
+};
+console.log(Object.keys(film));
+console.log(film.name);
+console.log(film.year);
+console.log(film.genre);
+console.log(film.casherfish);
+console.log(film.budget);
+console.log(film.director);
+console.log(film.rating);
+console.log(film.actors);
---
+++ radius/src/index.js
@@ -0,0 +1,9 @@
+
+const r = Math.floor(Math.random() * (100 - 10) + 10);
+const PI = 3.14;
+const s = r * r * PI;
+const l = 2 * PI * r;
+
+console.log(r);
+console.log(s);
+console.log(l);
